17ac79c109852625dd5cd2f7427479af2d899561
mariiafedorovaaa <113847443+mariiafedorovaaa@users.noreply.github.com>
2023-05-08 17:36:54 +0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -16,11 +16,13 @@ font = pygame.font.Font(None, 30)
 user_text_direction = ''
 user_text_value = ''
 input_rect1 = pygame.Rect(630, 340, 140, 32)
-input_rect2 = pygame.Rect(630, 380, 140, 32)
+input_rect2 = pygame.Rect(630, 430, 140, 32)
 # color_passive store color which is
 # color of input box.
 color_passive = pygame.Color('white')
 color = color_passive
+
+
 # Define the menu function
 def draw_menu():
     # Draw the menu background
@@ -28,24 +30,27 @@ def draw_menu():
     pygame.draw.rect(screen, menu_color, menu_rect)
 
     # Draw the cells in the upper part of the menu
+    global cell_rects
     cell_rects = []
     for i in range(3):
-        cell_rect = pygame.Rect(620, 50 + i*100, 160, 80)
+        cell_rect = pygame.Rect(620, 50 + i * 100, 160, 80)
         pygame.draw.rect(screen, cell_color, cell_rect)
-        cell_text = font.render("Cell {}".format(i+1), True, (255, 255, 255))
-        screen.blit(cell_text, (640, 70 + i*100))
+        cell_text = font.render("Cell {}".format(i + 1), True, (255, 255, 255))
+        screen.blit(cell_text, (640, 70 + i * 100))
         cell_rects.append(cell_rect)
 
     # Draw the settings in the lower part of the menu
     direction_text = font.render("Direction: ", True, (0, 0, 0))
     screen.blit(direction_text, (620, 340))
     amount_text = font.render("Value: ", True, (0, 0, 0))
-    screen.blit(amount_text, (620, 380))
+    screen.blit(amount_text, (620, 430))
 
     pygame.display.update()
 
+
 # Define the event handling function
 def handle_events():
+    draw_menu()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -62,30 +67,32 @@ def handle_events():
                 if event.type == pygame.KEYDOWN:
                     user_text_value += event.unicode
 
+
 # Main loop
 while True:
-    # Clear the screen and draw the menu
+    # Clear the screen
     screen.fill(background_color)
-    draw_menu()
-    pygame.draw.rect(screen, color, input_rect)
-    text_surface = base_font.render(user_text, True, (255, 255, 255))
+    # Handle events
+    pygame.draw.rect(screen, color, input_rect1)
+    text_surface1 = font.render(user_text_direction, True, (255, 255, 255))
+    pygame.draw.rect(screen, color, input_rect2)
+    text_surface2 = font.render(user_text_value, True, (255, 255, 255))
     # render at position stated in arguments
-    screen.blit(text_surface, (input_rect.x+5, input_rect.y+5))
-      
+    screen.blit(text_surface1, (input_rect1.x, input_rect1.y + 20))
+    screen.blit(text_surface2, (input_rect2.x, input_rect2.y + 20))
     # set width of textfield so that text cannot get
     # outside of user's text input
-    input_rect.w = max(100, text_surface.get_width()+10)
-      
+    input_rect1.w = max(100, text_surface1.get_width() + 10)
+    input_rect2.w = max(100, text_surface2.get_width() + 10)
     # display.flip() will update only a portion of the
     # screen to updated, not full area
-    pygame.display.flip()
-    # Handle events
     handle_events()
-    
+
+    pygame.display.flip()
+
 
     # Update the screen
     pygame.display.update()
 
     # Set the frame rate
     pygame.time.Clock().tick(60)
-   
